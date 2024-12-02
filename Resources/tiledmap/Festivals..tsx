67da7a3195d6be7e5e924c60dee82d6d0febb10e
xue haotian <hahaharveyx@outlook.com>
2024-12-03 00:54:26 +0800

    加入海滩、矿洞、农场、节日小镇地图

---
+++ Resources/tiledmap/Festivals..tsx
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Festivals." tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Festivals..png" width="512" height="512"/>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="428">
+  <animation>
+   <frame tileid="428" duration="100"/>
+   <frame tileid="430" duration="100"/>
+   <frame tileid="432" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="429">
+  <animation>
+   <frame tileid="429" duration="100"/>
+   <frame tileid="431" duration="100"/>
+   <frame tileid="433" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="460">
+  <animation>
+   <frame tileid="460" duration="100"/>
+   <frame tileid="462" duration="100"/>
+   <frame tileid="464" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="461">
+  <animation>
+   <frame tileid="461" duration="100"/>
+   <frame tileid="463" duration="100"/>
+   <frame tileid="465" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="492">
+  <animation>
+   <frame tileid="492" duration="100"/>
+   <frame tileid="494" duration="100"/>
+   <frame tileid="495" duration="100"/>
+   <frame tileid="496" duration="100"/>
+   <frame tileid="497" duration="100"/>
+   <frame tileid="496" duration="100"/>
+   <frame tileid="495" duration="100"/>
+   <frame tileid="494" duration="100"/>
+   <frame tileid="492" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Resources/tiledmap/spring_beach..tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="spring_beach." tilewidth="16" tileheight="16" tilecount="527" columns="17">
+ <image source="spring_beach..png" width="272" height="496"/>
+ <tile id="141">
+  <animation>
+   <frame tileid="141" duration="200"/>
+   <frame tileid="142" duration="200"/>
+   <frame tileid="143" duration="200"/>
+   <frame tileid="144" duration="200"/>
+   <frame tileid="145" duration="200"/>
+   <frame tileid="146" duration="200"/>
+   <frame tileid="147" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="158">
+  <animation>
+   <frame tileid="158" duration="200"/>
+   <frame tileid="159" duration="200"/>
+   <frame tileid="160" duration="200"/>
+   <frame tileid="161" duration="200"/>
+   <frame tileid="162" duration="200"/>
+   <frame tileid="163" duration="200"/>
+   <frame tileid="164" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="175">
+  <animation>
+   <frame tileid="175" duration="200"/>
+   <frame tileid="176" duration="200"/>
+   <frame tileid="177" duration="200"/>
+   <frame tileid="178" duration="200"/>
+   <frame tileid="179" duration="200"/>
+   <frame tileid="180" duration="200"/>
+   <frame tileid="181" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="192">
+  <animation>
+   <frame tileid="192" duration="200"/>
+   <frame tileid="193" duration="200"/>
+   <frame tileid="194" duration="200"/>
+   <frame tileid="195" duration="200"/>
+   <frame tileid="196" duration="200"/>
+   <frame tileid="197" duration="200"/>
+   <frame tileid="198" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="209">
+  <animation>
+   <frame tileid="209" duration="200"/>
+   <frame tileid="210" duration="200"/>
+   <frame tileid="211" duration="200"/>
+   <frame tileid="212" duration="200"/>
+   <frame tileid="213" duration="200"/>
+   <frame tileid="214" duration="200"/>
+   <frame tileid="215" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="226">
+  <animation>
+   <frame tileid="226" duration="200"/>
+   <frame tileid="227" duration="200"/>
+   <frame tileid="228" duration="200"/>
+   <frame tileid="229" duration="200"/>
+   <frame tileid="230" duration="200"/>
+   <frame tileid="231" duration="200"/>
+   <frame tileid="232" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="243">
+  <animation>
+   <frame tileid="243" duration="200"/>
+   <frame tileid="244" duration="200"/>
+   <frame tileid="245" duration="200"/>
+   <frame tileid="246" duration="200"/>
+   <frame tileid="247" duration="200"/>
+   <frame tileid="248" duration="200"/>
+   <frame tileid="249" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="260">
+  <animation>
+   <frame tileid="260" duration="200"/>
+   <frame tileid="261" duration="200"/>
+   <frame tileid="262" duration="200"/>
+   <frame tileid="263" duration="200"/>
+   <frame tileid="264" duration="200"/>
+   <frame tileid="265" duration="200"/>
+   <frame tileid="266" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
