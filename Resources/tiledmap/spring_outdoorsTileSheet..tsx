0481b96ba5f3af9daacfa7d8ae29857ac9a7e28d
xue haotian <hahaharveyx@outlook.com>
2024-12-05 15:44:38 +0800

    加入玩家房子地图，修改地图压缩方式
    修复了同一图层有不同图集的图块的显示问题，修改了地图压缩方式使其显示正确，完成了对瓦片地图的最终修改

--- Resources/tiledmap/spring_outdoorsTileSheet..tsx
+++ Resources/tiledmap/spring_outdoorsTileSheet..tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="spring_outdoorsTileSheet." tilewidth="16" tileheight="16" tilecount="1975" columns="25">
  <image source="spring_outdoorsTileSheet..png" width="400" height="1264"/>
+ <tile id="420">
+  <animation>
+   <frame tileid="420" duration="200"/>
+   <frame tileid="421" duration="200"/>
+   <frame tileid="422" duration="200"/>
+   <frame tileid="423" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1115">
+  <animation>
+   <frame tileid="1115" duration="200"/>
+   <frame tileid="1117" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1116">
+  <animation>
+   <frame tileid="1116" duration="200"/>
+   <frame tileid="1118" duration="200"/>
+  </animation>
+ </tile>
 </tileset>
